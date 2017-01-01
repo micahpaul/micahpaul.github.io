0b84bac7b2891502d18bdec2e7529b233e6067be
micahpaul <mhawkinson@kalos-inc.com>
2016-12-31 21:32:22 -0600

    Update ZedsColors.js

--- zed/js/ZedsColors.js
+++ zed/js/ZedsColors.js
@@ -73,9 +73,9 @@ function sayTheColor() {
 }
 
 function main() {
-	//var welcome = new Audio("sounds/welcome.mp3");
-	//welcome.onended = reset;
-	//welcome.play();
+	var welcome = new Audio("sounds/welcome.mp3");
+	welcome.onended = reset;
+	welcome.play();
 	reset();
 }
 
